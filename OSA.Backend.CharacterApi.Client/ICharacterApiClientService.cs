b0feab93d0e943b06a55d534a752c315d1132fc4
blakeneybk <blakeneybk@gmail.com>
2023-11-08 16:32:49 -0800

    forgot the interfaces

--- OSA.Backend.CharacterApi.Client/ICharacterApiClientService.cs
+++ OSA.Backend.CharacterApi.Client/ICharacterApiClientService.cs
@@ -1,6 +1,13 @@
-﻿namespace OSA.Backend.CharacterApi.Client
+﻿using OSA.Backend.CharacterApi.Client.DtoModels;
+
+namespace OSA.Backend.CharacterApi.Client
 {
     public interface ICharacterApiClientService
     {
+        Task<IEnumerable<StarTrekCharacterDto>?> GetAllCharactersAsync();
+        Task<StarTrekCharacterDto?> GetCharacterAsync(int id);
+        Task<StarTrekCharacterDto?> AddCharacterAsync(StarTrekCharacterDto character);
+        Task<bool> UpdateCharacterAsync(int id, StarTrekCharacterDto character);
+        Task<bool> DeleteCharacterAsync(int id);
     }
 }
--- OSA.Backend.StarshipApi.Client/IStarshipApiClientService.cs
+++ OSA.Backend.StarshipApi.Client/IStarshipApiClientService.cs
@@ -1,6 +1,13 @@
-﻿namespace OSA.Backend.StarshipApi.Client
+﻿using OSA.Backend.StarshipApi.Client.DtoModels;
+
+namespace OSA.Backend.StarshipApi.Client
 {
     public interface IStarshipApiClientService
     {
+        Task<IEnumerable<StarTrekStarshipDto>?> GetAllStarshipsAsync();
+        Task<StarTrekStarshipDto?> GetStarshipAsync(int id);
+        Task<StarTrekStarshipDto?> AddStarshipAsync(StarTrekStarshipDto starship);
+        Task<bool> UpdateStarshipAsync(int id, StarTrekStarshipDto starship);
+        Task<bool> DeleteStarshipAsync(int id);
     }
 }
